db548b5862103055b864b4543a82bb88fb1f5dc2
liis lehemets <liis.lehemets@tktk.ee>
2024-12-11 20:07:03 +0200

    add week2 code

---
+++ UserAgeValidation/Program.cs
@@ -0,0 +1,41 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Threading.Channels;
+
+Console.WriteLine("Hello, Liis!");
+//rakendus küsib kasutajalt valida tema sugu (m/f)
+//rakendus küsib kasutajal sisestada tema perekonnanime
+//lähtudes kasutaja valikust, rakendus tervitab kasutajat järgmiselt:
+// "Welcome, MR. [kasutaja perekonnanimi] /"Welcome, Ms. [kasutaja perekonnanimi]"
+
+Console.WriteLine("Please, select your gender (m/f):");
+
+char userGender = Char.Parse (Console.ReadLine()); //loeb konsoolist maha andmeid string (sõne) formaadis.
+//chari tahame seetõttu, et vaid m ja F väärtuseid salvestada, pole vaja suuri sõnu ja vahemälu.
+
+Console.WriteLine("Please, enter your last name:");
+string userLastName= Console.ReadLine();
+
+//miks peab küsima kasutaja nime enne if koodi? Kuna neid andmeid ei ole veel antud, kood jookseb ülevalt alla ja if ei leiaks neid muidu.
+
+if (userGender == 'm')
+{
+    Console.WriteLine($"Welcome, Mr.{userLastName}!");
+}
+else if (userGender == 'f')
+{
+    Console.WriteLine($"Welcome, MS.{userLastName}!");
+}
+else
+{
+    Console.WriteLine($"Welcome, {userLastName} !");
+}
+
+// kontrollin kas kasutaja sugu = m
+// 1 = toimuks uue väärtuse salvestamine. If lausetes tohib vaid topelt == kasutada
+//{ sulgude abil pannakse tööle see plokk ja kui on käivitatud, siis vahemälust kustutakse. 
+// " on sõnadele ja ' on char infole mõeldud- sedasi teeb süsteem vahet
+//else on kõik muud juhud mis ei ole m või f
+// else if ja if vahe - sest kui juba korra on if kontroll käinud,
+// //siis ei tohiks koormata arvutit ja tuleks edasi else ifiga minna
+
+Console.WriteLine($"Welcome, {userGender}");
---
+++ UserAgeValidation1/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//rakendus küsib kasutaja vanust
+//kui kasutaja vanus on väiksem kui 13, siis konsoolis kuvatakase
+//"you are too young to use Instagram"
+//muul juhul kuvatakse
+//"Welcome to Instagram"
+
+Console.WriteLine("Enter your age: ");
+
+int userAge= Int32.Parse (Console.ReadLine()); //"13" - heap, 13- salvestatakse stacki ja on kiirem, säästab ressurssi
+//console tagastab vaid stringi siis tuleb parsida või konsolideerida
+//int tüüpi muutuja salvestatakse ka stacki
+//string muutuja puhul ei laseks seda teha, sõnu ja arve ei saa võrrelda. Otsime vastuseks numbrit seega peab olema arv
+if (userAge >= 13) // kui kasutaja vanus on suurem või võrdne siis toimib sedasi
+{
+    Console.WriteLine("Welcome to Instagram!");
+}
+
+else //viimane võimalik valik arvuti jaoks kui on üle 13 vastus
+{
+    Console.WriteLine("You are too young to use Instagram.");
+}
